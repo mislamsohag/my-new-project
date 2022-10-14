0a0897c72853c94218d6150487dbe740fb4956ef
MD. MAZHARUL ISLAM SOHAG <55456408+mislamsohag@users.noreply.github.com>
2022-10-14 23:11:20 +0600

    lesson 5 first commit

---
+++ src/componants/pages/Signup.js
@@ -0,0 +1,12 @@
+
+export default function Signou() {
+    return (
+        <>
+            <h1>Create an account</h1>
+            <div className='column'>
+                <Illastration />
+                <Form />
+            </div>
+        </>
+    );
+}
