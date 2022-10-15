a345b784edc0f42acd8da8d6fd4abdc4598f2f5e
MD. MAZHARUL ISLAM SOHAG <55456408+mislamsohag@users.noreply.github.com>
2022-10-15 15:40:35 +0600

    lesson 5 compleate

--- src/componants/App.js
+++ src/componants/App.js
@@ -1,11 +1,13 @@
 import "../styles/App.css"
 import Layout from "./Layout";
-import Home from "./pages/Home";
+import Signoup from "./pages/Signup";
+// import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Home />
+        {/* <Home /> */}
+        <Signoup />
       </Layout>
     </div>
   );
--- src/componants/pages/Signup.js
+++ src/componants/pages/Signup.js
@@ -1,11 +1,35 @@
+import Form from '../Form';
+import Illustration from '../Illustration';
+import classes from '../../styles/Signup.module.css';
+import TextInput from '../TextInput';
+import Checkbox from '../Checkbox';
+import Button from '../Button';
 
-export default function Signou() {
+
+export default function Signoup() {
     return (
         <>
             <h1>Create an account</h1>
             <div className='column'>
-                <Illastration />
-                <Form />
+                <Illustration />
+                <Form className={classes.signup}>
+                    <TextInput type="text" placeholder="Enter Your Name" icon="person" />
+
+                    <TextInput type="email" placeholder="Enter Email" icon="alternate_email" />
+
+                    <TextInput type="password" placeholder="Enter password" icon="lock" />
+
+                    <TextInput type="password" placeholder="Confirm password" icon="lock_clock" />
+
+
+                    <Checkbox text="I agree to the Terms &amp; Conditions" />
+
+                    <Button>Submit now</Button>
+
+                    <div className="info">
+                        Already have an account? <a href="login.html">Login</a> instead.
+                    </div>
+                </Form>
             </div>
         </>
     );
