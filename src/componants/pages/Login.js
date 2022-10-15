c10e6b06763e10786819db011050313b081dca8b
MD. MAZHARUL ISLAM SOHAG <55456408+mislamsohag@users.noreply.github.com>
2022-10-15 20:34:42 +0600

    lesson 6 first commit

---
+++ src/componants/pages/Login.js
@@ -0,0 +1,13 @@
+import Illustration from "../Illustration";
+
+export default function Loging() {
+    return (
+        <>
+            <h1>Login to your account</h1>
+            <div className="column">
+                <Illustration />
+
+            </div>
+        </>
+    );
+}
