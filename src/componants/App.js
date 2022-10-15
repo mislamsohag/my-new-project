36324327301d7ff310839788018de3b4935cc16c
MD. MAZHARUL ISLAM SOHAG <55456408+mislamsohag@users.noreply.github.com>
2022-10-15 22:52:53 +0600

    Login Page Done

--- src/componants/App.js
+++ src/componants/App.js
@@ -1,13 +1,16 @@
 import "../styles/App.css"
 import Layout from "./Layout";
-import Signoup from "./pages/Signup";
+import Loging from "./pages/Login";
+// import Signoup from "./pages/Signup";
 // import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <Layout>
         {/* <Home /> */}
-        <Signoup />
+        {/* <Signoup /> */}
+        <Loging />
       </Layout>
     </div>
   );
--- src/componants/pages/Login.js
+++ src/componants/pages/Login.js
@@ -1,12 +1,24 @@
 import Illustration from "../Illustration";
-
+import classes from '../../styles/Login.module.css';
+import TextInput from '../TextInput';
+import Button from "../Button";
+import Form from "../Form";
 export default function Loging() {
     return (
         <>
             <h1>Login to your account</h1>
             <div className="column">
                 <Illustration />
+                <Form className={classes.login} >
 
+                    <TextInput type="email" placeholder="Enter email" icon="alternate_email" />
+
+                    <TextInput type="password" placeholder="Enter password" icon="lock" />
+
+                    <Button> Submit now </Button>
+
+                    <div className="info">Don't have an account? <a href="signup.html">Signup</a> instead.</div>
+                </Form>
             </div>
         </>
     );
