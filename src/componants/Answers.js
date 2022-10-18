5352b6c191b68a44d89b609806643437f4027f43
MD. MAZHARUL ISLAM SOHAG <55456408+mislamsohag@users.noreply.github.com>
2022-10-18 19:14:00 +0600

    Lesson 7 First commit

---
+++ src/componants/Answers.js
@@ -0,0 +1,11 @@
+import classes from '../styles/Answers.modules.css';
+
+export default function Answers() {
+    return (
+        <div className={classes.answers}>
+
+            <Checkbox className={classes.answer} />
+
+        </div>
+    );
+}
