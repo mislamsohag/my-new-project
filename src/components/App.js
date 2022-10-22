fd7b22f9e397e022a44a330236d1b9c6847dccdc
MD. MAZHARUL ISLAM SOHAG <55456408+mislamsohag@users.noreply.github.com>
2022-10-22 22:36:35 +0600

    Auth Context Provider Complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,22 @@ import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
+import { AuthProvider } from "../context/AuthContext";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/quiz" component={Quiz} />
-          <Route exact path="/result" component={Result} />
-        </Switch>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/quiz" component={Quiz} />
+            <Route exact path="/result" component={Result} />
+          </Switch>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
